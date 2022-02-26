da9cae0505772b7ffa613ab8b9893f1001cd110a
Jacques Sham <jacquessham2015@gmail.com>
2022-02-26 14:29:05 -0800

    Fix binary search bug

--- Searching/Ruby/BinarySearch.rb
+++ Searching/Ruby/BinarySearch.rb
@@ -5,11 +5,11 @@ def BinarySearch(arr, target)
 	num_max = arr.length
 	while num_min <= num_max do
 		mid = ((num_min+num_max)/2).to_i
-		if arr[mid] == target then return arr[mid] end
+		if arr[mid] == target then return mid end
 		if target > arr[mid] 
-			then num_min = mid
+			then num_min = mid + 1
 		else
-			num_max = mid
+			num_max = mid - 1
 		end # end if
 	end # end while
 	return -1
